7c396086c0a386a5b4d42840957be31a9e6f9290
Seynax <seyronax@gmail.com>
2023-06-29 18:07:05 +0200

    Main corrections

--- src/main/java/fr/seynax/universecore/UniverseCore.java
+++ src/main/java/fr/seynax/universecore/UniverseCore.java
@@ -2,27 +2,21 @@ package fr.seynax.universecore;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-
-@Mod(UniverseCore.MODID)
 public class UniverseCore
 {
-    public static final String MODID = "universecore";
-	// private static final Logger LOGGER = LogUtils.getLogger();
 	private static  DeferredRegister<Block> BLOCKS;
 
-    public UniverseCore()
-    {
-        // MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    public final static void init(String modidIn, IEventBus modEventBusIn)
+    public final static void init(String modidIn)
     {
     	BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, modidIn);
+    }
+
+    public final static void register(IEventBus modEventBusIn)
+    {
   	  	BLOCKS.register(modEventBusIn);
     }
 
