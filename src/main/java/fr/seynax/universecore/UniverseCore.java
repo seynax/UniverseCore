6a907595bbf1d0bc8c150086b859e3f763d80373
Seynax <seyronax@gmail.com>
2023-06-29 18:28:31 +0200

    Registry test corrections

--- src/main/java/fr/seynax/universecore/UniverseCore.java
+++ src/main/java/fr/seynax/universecore/UniverseCore.java
@@ -1,14 +1,30 @@
 package fr.seynax.universecore;
 
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+
+@Mod(UniverseCore.MODID)
 public class UniverseCore
 {
+    public static final String MODID = "universecore";
+	private static final Logger LOGGER = LogUtils.getLogger();
+
 	private static  DeferredRegister<Block> BLOCKS;
+
+    public UniverseCore()
+    {
+         MinecraftForge.EVENT_BUS.register(this);
+    }
 
     public final static void init(String modidIn)
     {
