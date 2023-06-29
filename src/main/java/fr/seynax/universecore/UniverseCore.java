5ec2cbbf0e162fab92755556ecf03e72234fdad8
Seynax <seyronax@gmail.com>
2023-06-29 17:47:36 +0200

    Main corrections

--- src/main/java/fr/seynax/universecore/UniverseCore.java
+++ src/main/java/fr/seynax/universecore/UniverseCore.java
@@ -6,12 +6,8 @@ import com.mojang.logging.LogUtils;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -30,36 +26,17 @@ public class UniverseCore
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
   	  	BLOCKS.register(modEventBus);
-        modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    public void init(String modidIn)
+    public final static void init(String modidIn)
     {
     	BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, modidIn);
     }
 
-    public RegistryObject<Block> register(String idIn, Block blockIn)
+    public final static RegistryObject<Block> register(String idIn, Block blockIn)
     {
     	return BLOCKS.register(idIn, () -> blockIn);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-    }
-
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-        }
     }
 }
